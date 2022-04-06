e6cb7f98312bc226e4aeea261473934ada64f87d
tomluxton <ta.luxton@gmail.com>
2022-04-06 12:24:35 +1000

    Added  working navigation to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
-import {useNavigation, useLocation} from 'react-router-dom'
+import {useNavigate, useLocation} from 'react-router-dom'
 import {ReactComponent as OfferIcon} from "../assets/svg/localOfferIcon.svg";
 import {ReactComponent as ExploreIcon} from "../assets/svg/exploreIcon.svg";
 import {ReactComponent as PersonOutlineIcon} from "../assets/svg/personOutlineIcon.svg";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <footer className = "navbar">
       <nav className = "navbarNav">
         <ul className = "navbarListItems">
-          <li className = "navbarListItem">
+          <li className = "navbarListItem" onClick = {() => navigate("/")}>
             <ExploreIcon fill ="#2c2c2c" width = "36pm" height = "36px" />
             <p>Explore</p>
           </li>
-          <li className = "navbarListItem">
+          <li className = "navbarListItem" onClick = {() => navigate("/offers")}>
             <OfferIcon fill ="#2c2c2c" width = "36pm" height = "36px" />
             <p>Offer</p>
           </li>
-          <li className = "navbarListItem">
+          <li className = "navbarListItem" onClick = {() => navigate("/profile")}>
             <PersonOutlineIcon fill ="#2c2c2c" width = "36pm" height = "36px" />
             <p>Profile</p>
           </li>
