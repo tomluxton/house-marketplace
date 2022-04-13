8453dfeaacdf776726e156ad155e7ca1afad093e
tomluxton <ta.luxton@gmail.com>
2022-04-13 12:01:32 +1000

    Singup works

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,10 @@
 import {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {db} from "../firebase.config";
 import {ReactComponent as ArrowRightIcon} from "../assets/svg/keyboardArrowRightIcon.svg";
-import visibilityIcon from "../assets/svg/visibilityIcon.svg"
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import {setDoc, doc, serverTimestamp} from "firebase/firestore";
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -22,6 +25,37 @@ function SignUp() {
     }))
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword
+      (
+        auth,
+        email,
+        password
+      )
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+
+      const formDataCopy = {...formData}
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
+
+      navigate("/");      
+    } catch (error){
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -29,7 +63,7 @@ function SignUp() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input type="text" className="nameInput" placeholder="Name" id="name" value={name} onChange= {onChange} />
 
           <input type = "email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
