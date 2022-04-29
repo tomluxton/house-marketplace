534c9ee07ec86c277bc0602d5f623a26f1c80a2c
tomluxton <ta.luxton@gmail.com>
2022-04-29 16:19:11 +1000

    Added toastify for error popups, added signout, and update user details. Cant update email as that is part of firebase authentication

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {toast} from "react-toastify";
 import {Link, useNavigate} from "react-router-dom";
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
 import {db} from "../firebase.config";
@@ -31,8 +32,7 @@ function SignUp() {
     try {
       const auth = getAuth();
 
-      const userCredential = await createUserWithEmailAndPassword
-      (
+      const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
@@ -52,7 +52,7 @@ function SignUp() {
 
       navigate("/");      
     } catch (error){
-      console.log(error);
+      toast.error("Something went wrong with registration");
     }
   }
 
