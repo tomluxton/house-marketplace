96f611d63ad347598e0cb42475b5a4284ebb49cb
tomluxton <ta.luxton@gmail.com>
2022-04-30 13:41:12 +1000

    Enabled private route for staying logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Explore from "./pages/Explore";
@@ -9,6 +9,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />}/>
           <Route path="/offers" element={<Offers />}/>
-          <Route path="/profile" element={<Profile />}/>
+          <Route path = '/profile' element = {<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />}/>
+          </Route>
           <Route path="/sign-in" element={<SignIn />}/>
           <Route path="/sign-up" element={<SignUp />}/>
           <Route path="/forgot-password" element={<ForgotPassword />}/>
