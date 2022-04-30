e6d11714d0f0d0d28677b87da087b1f8eef78aa0
tomluxton <ta.luxton@gmail.com>
2022-04-30 15:34:28 +1000

    Added google authentication to the app

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -54,7 +54,7 @@ function Profile() {
   return <div className = 'profile'>
     <header className = "profileHeader">
       <p className = "pageHeader"> My Profile</p>
-      <button type = "button" className="logout" onClick = {onLogout}>
+      <button type = "button" className="logOut" onClick = {onLogout}>
         Logout
       </button>
     </header>
